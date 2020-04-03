eb06c9ae4caac1489a16209d5a73d1f2869bd9c7
laranja14 <laranja1419@gmail.com>
2020-04-03 22:35:56 +0900

    Updates

---
+++ precache-manifest.b418e2b46e37e2def0a3676c8bd90be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc28d8ab2398ec1447625375b33bdc84",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f893712007b8be027ec9",
+    "url": "/movie_app_2020/static/css/main.845d79c5.chunk.css"
+  },
+  {
+    "revision": "2731f645873efd1058d0",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f893712007b8be027ec9",
+    "url": "/movie_app_2020/static/js/main.781f43b9.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
